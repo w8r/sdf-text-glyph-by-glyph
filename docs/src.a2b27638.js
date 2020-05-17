6a8e83aaaa78933258b9ebf647a889b4f1e5db32
Alexander Milevski <info@w8r.name>
2020-05-17 22:31:59 +0200

    gh-pages

--- docs/src.a2b27638.js
+++ docs/src.a2b27638.js
@@ -25107,7 +25107,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50768" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62641" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -25284,4 +25284,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
-//# sourceMappingURL=/src.a2b27638.js.map
+//# sourceMappingURL=src.a2b27638.js.map
